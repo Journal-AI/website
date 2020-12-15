bbd0110f40c0d36e8d99900a7d2c0e317c2e6b38
Aswin Vijayakumar <aswinkvj@gmail.com>
2020-12-15 13:33:54 +0000

    pages commit as well as styles added to the page

---
+++ pages/contact_us/contact_us.php
@@ -0,0 +1,5 @@
+<div class="clearfix">
+    <!-- <h4 class="text-capitalize heading">Get in touch with us:</h4> -->
+    <?php // require_once "contactform.tpl.php"; ?>
+    <?php // require_once PAGE_ROOT . "/video/include.html"; ?>
+</div>
---
+++ pages/home/showcase.tpl.php
@@ -0,0 +1,16 @@
+<div class="container section-page-file" id="showcase">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 center-block boxed-screenshots" id="boxed-screenshots">
+        <div class="col-lg-4 img-thumbnail bg-img-game img-screenshot" id="games-container">
+            <div class="bg-img-above-half">Games with GIS based Maps</div>
+            <div class="bg-img-below-half">Board Games</div>
+        </div>
+        <div class="col-lg-4 img-thumbnail bg-img-dashcam img-screenshot" id="dashcam-container">
+            <div class="bg-img-above-half">FIT.exe</div>
+            <div class="bg-img-below-half">Dashcam</div>
+        </div>
+        <div class="col-lg-4 img-thumbnail bg-img-privilege img-screenshot" id="privilege-container">
+            <div class="bg-img-above-half">Privilege Networks</div>
+            <div class="bg-img-below-half">Location Finder</div>
+        </div>
+    </div>
+</div>
