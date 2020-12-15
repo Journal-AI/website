fa8a11b823f13f1496274e0fe44f00e6c0d4972c
Aswin Vijayakumar <aswinkvj@gmail.com>
2020-12-15 14:13:32 +0000

    style added page modified to contain description

--- pages/home/description.tpl.php
+++ pages/home/description.tpl.php
@@ -1,3 +1,39 @@
-<div class="container section-page-file">
-    
+<div class="container section-page-file" id="description">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 center-block">
+        <div class="col-lg-4 img-thumbnail img-screenshot" id="games-description-1">
+            <p class="paragraph-description">
+                Web based GIS Games entertain people. 
+            </p>
+        </div>
+        <div class="col-lg-4 img-thumbnail img-screenshot" id="dashcam-description-1">
+            <p class="paragraph-description">
+                FIT.exe is an educational tool that helps prioritizing features. 
+            </p>
+        </div>
+        <div class="col-lg-4 img-thumbnail img-screenshot" id="privilege-description-1">
+            <p class="paragraph-description">
+                Privilege Networks help people available for discounted consumer goods.
+            </p>
+        </div>
+    </div>
+</div>
+<hr />
+<div class="container section-page-file" id="description">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 center-block">
+        <div class="col-lg-4 img-thumbnail img-screenshot" id="games-description-2">
+            <p class="paragraph-description">
+                Board Games promote sustainability within people. 
+            </p>
+        </div>
+        <div class="col-lg-4 img-thumbnail img-screenshot" id="dashcam-description-2">
+            <p class="paragraph-description">
+                Dashcam provides comfortable journey by promoting safe streets.
+            </p>
+        </div>
+        <div class="col-lg-4 img-thumbnail img-screenshot" id="privilege-description-2">
+            <p class="paragraph-description">
+                Location Finder helps kids produce code by finding geo-coordinates.
+            </p>
+        </div>
+    </div>
 </div>
