19f9cc2de586cd5fddf3c14f3e03f00f41897826
Aswin Vijayakumar <aswinkvj@gmail.com>
2020-12-15 14:49:05 +0000

    button added and styled to text align

--- pages/home/showcase.tpl.php
+++ pages/home/showcase.tpl.php
@@ -1,16 +1,19 @@
 <div class="container section-page-file" id="showcase">
     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-10 center-block boxed-screenshots" id="boxed-screenshots">
         <div class="col-lg-4 img-thumbnail bg-img-game img-screenshot" id="games-container">
-            <div class="bg-img-above-half">Games with GIS based Maps</div>
-            <div class="bg-img-below-half">Board Games</div>
+            <div class="bg-img-above-half"><p class="container-description">Games with GIS based Maps</p></div>
+            <div class="bg-img-below-half"><p>Board Games</p></div>
         </div>
         <div class="col-lg-4 img-thumbnail bg-img-dashcam img-screenshot" id="dashcam-container">
-            <div class="bg-img-above-half">FIT.exe</div>
-            <div class="bg-img-below-half">Dashcam</div>
+            <div class="bg-img-above-half"><p class="container-description">FIT.exe</p>
+            <p class="container-link"><a href="#" class="btn-primary">Download FIT.exe</a></p>
+            </div>
+            <div class="bg-img-below-half"><p class="container-description">Dashcam</p></div>
         </div>
         <div class="col-lg-4 img-thumbnail bg-img-privilege img-screenshot" id="privilege-container">
-            <div class="bg-img-above-half">Privilege Networks</div>
-            <div class="bg-img-below-half">Location Finder</div>
+            <div class="bg-img-above-half"><p class="container-description">Privilege Networks</p></div>
+            <div class="bg-img-below-half"><p class="container-description">Location Finder</p>
+            <p class="container-link"><a href="#" class="btn-primary">Try the App</a></p></div>
         </div>
     </div>
 </div>
