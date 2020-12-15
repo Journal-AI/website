966f6f39a6d98514fb39b0ee395ef71d24a068bf
Aswin Vijayakumar <aswinkvj@gmail.com>
2020-12-15 14:26:50 +0000

    descriptions added

--- pages/home/description.tpl.php
+++ pages/home/description.tpl.php
@@ -4,15 +4,30 @@
             <p class="paragraph-description">
                 Web based GIS Games entertain people. 
             </p>
+            <p class="paragraph-reference">
+                <a class="link" href="https://journalai.buzz/2020/10/06/world-aerosol-content-for-sustainability-games/">
+                    World Aerosol Content for Sustainability Games
+                </a>
+            </p>
         </div>
         <div class="col-lg-4 img-thumbnail img-screenshot" id="dashcam-description-1">
             <p class="paragraph-description">
                 FIT.exe is an educational tool that helps prioritizing features. 
             </p>
+            <p class="paragraph-reference">
+                <a class="link" href="https://www.analyticsvidhya.com/blog/2020/10/boxing-and-unboxing-of-statistical-models-with-gaussian-learning/">
+                    Boxing and Unboxing of Statistical Models With Gaussian Learning
+                </a>
+            </p>
         </div>
         <div class="col-lg-4 img-thumbnail img-screenshot" id="privilege-description-1">
             <p class="paragraph-description">
                 Privilege Networks help people available for discounted consumer goods.
+            </p>
+            <p class="paragraph-reference">
+                <a class="link" href="https://journalai.buzz/2020/11/24/privilege-networks-for-enhanced-usability/">
+                    What are Privilege Networks?
+                </a>
             </p>
         </div>
     </div>
@@ -24,15 +39,30 @@
             <p class="paragraph-description">
                 Board Games promote sustainability within people. 
             </p>
+            <p class="paragraph-reference">
+                <a class="link" href="https://journalai.buzz/2020/10/06/jumble-the-passage-theme-for-sustainability-games/">
+                    Jumble the Passage, Theme
+                </a>
+            </p>
         </div>
         <div class="col-lg-4 img-thumbnail img-screenshot" id="dashcam-description-2">
             <p class="paragraph-description">
                 Dashcam provides comfortable journey by promoting safe streets.
             </p>
+            <p class="paragraph-reference">
+                <a class="link" href="https://journalai.buzz/2020/10/19/dashcam-traceability-for-driving-risk-avoidance/">
+                    Dashcam Traceability - For Driving Risk Avoidance
+                </a>
+            </p>
         </div>
         <div class="col-lg-4 img-thumbnail img-screenshot" id="privilege-description-2">
             <p class="paragraph-description">
                 Location Finder helps kids produce code by finding geo-coordinates.
+            </p>
+            <p class="paragraph-reference">
+                <a class="link" href="">
+                    Try this App! produced by @Journal-AI
+                </a>
             </p>
         </div>
     </div>
